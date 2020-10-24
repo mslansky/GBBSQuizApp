63e2856061d3be727156544215d7de71ca2f47cb
Madison Slansky <madisonslansky@Madisons-Air.attlocal.net>
2020-10-24 18:01:19 -0500

    modifiedCSS

--- app.js
+++ app.js
@@ -4,14 +4,39 @@
 const store = {
   // 5 or more questions are required
   questions: [{
-    question: 'What color is broccoli?',
-    answers: ['red', 'orange', 'pink', 'green'],
-    correctAnswer: 'green',
+    question: 'What is the name of The Great British Baking Show in England?',
+    answers: ['The Great English Baking Show', 'The Great British Bake Off', 'Greatest British Bake Off Show', 'The English Bake Off'],
+    correctAnswer: 'The Great British Bake Off',
   },
   {
-    question: 'What is the current year?',
-    answers: ['1970', '2015', '2019', '2005'],
-    correctAnswer: '2019',
+    question: 'How do they test the ovens for consistency?',
+    answers: ['Each oven is tested with an oven thermometer', 'Once a week the ovens are maintenced by a professional', 'Every day, every oven is used to bake a Victoria sponge cake', 'They expect the contestants to regulate thier own ovens'],
+    correctAnswer: 'Every day, every oven is used to bake a Victoria sponge cake',
+  },
+  {
+    question: 'Prospective contestant bakers must fill out an application with how many pages?',
+    answers: ['Seven', 'Thirteen', 'Four', 'Twenty-One'],
+    correctAnswer: 'Seven',
+  },
+  {
+    question: 'How are the ingredients for each bake-off paid for?',
+    answers: ['The show production company reimburses the bakers', 'The show crowd-funds for the cost of ingredients', 'The ingredients are gifted by a sponsored bakery', 'The contestents pay for the ingredients they use on thier own'],
+    correctAnswer: 'The contestents pay for the ingredients they use on thier own',
+  },
+  {
+    question: 'Last season, how many eggs in total did the bakers use?',
+    answers: ['490', '1,600', '900', '2,500'],
+    correctAnswer: '1,600',
+  },
+  {
+    question: 'What is a soggy bottom?',
+    answers: ['An undercooked bottom portion of your pastry', 'Leaky filling ruining your dessert', 'Tea cakes soaked in brandy', 'A tasty British treat'],
+    correctAnswer: 'An undercooked bottom portion of your pastry',
+  },
+  {
+    question: 'What is the technical challenge?',
+    answers: ['A challenge where bakers have to deconstruct the ingredients of a pastry given to them', 'A challenge where the bakers can make anything they want', 'A challenge where bakers have to follow a recipe with missing instructions', 'A challenge for the judges to complete to show thier skills'],
+    correctAnswer: 'A challenge where bakers have to follow a recipe with missing instructions',
   },
   ],
   quizStarted: false,
@@ -42,12 +67,12 @@ function generateStartPage() {
   <div class="startPage">
   <h1>Welcome to the Great British Baking Show Quiz!</h1>
   <h2>Do you have what it takes to be starbaker?</h2>
-  <section class="startPageFlex">
+  <section class="flexboxImages">
       <div class="startPageImages">
-          <div class="Paul&Prue">
-              <img src="paul.png" alt="Image of Paul Hollywood"></div>
-          <div class="Paul&Prue">
-              <img src="prue2.png" alt="Image of Prue Leith"></div>
+          <div class="PaulPrue">
+              <img src="/paul.png" alt="Image of Paul Hollywood"></div>
+          <div class="PaulPrue">
+              <img src="/prue2.png" alt="Image of Prue Leith"></div>
       </div>
   </section>
   <div class="start-button-center">
@@ -55,7 +80,8 @@ function generateStartPage() {
   <span class="button-label">Ready...Bakers...Start!</span>
   </button>
   </div>
-</div>`;
+  <br/>
+</div>`
   return startPage;
 }
 
@@ -66,39 +92,71 @@ function generateQuestionPage() {
   questionCount++;
   let questionPage = `
 <div class="card">
-<p>Score:${store.score}</p>
-<p>Question:${questionCount}/${store.questions.length}</p>
+<div class="counters">
+<p> Your Score : ${store.score}</p>
+<p> Question Number : ${questionCount} / ${store.questions.length}</p>
+</div>
   <h2>${question.question}</h2>
+  <br/>
  <form>
-      <label> ${question.answers[0]}</label>
+      <div class="questionFormat">
       <input type="radio" name="answer" value="${question.answers[0]}">
-      <br>
-      <label> ${question.answers[1]}</label>
+      <label> ${question.answers[0]}</label>
+      </div>
+      <br/>
+
+      <div class="questionFormat">
       <input type="radio" name="answer" value="${question.answers[1]}">
-      <br>
-      <label> ${question.answers[2]}</label>
+      <label> ${question.answers[1]}</label>
+      </div>
+      <br/>
+
+      <div class="questionFormat">
       <input type="radio" name="answer" value="${question.answers[2]}">
-      <br>
-      <label> ${question.answers[3]}</label>
+      <label> ${question.answers[2]}</label>
+      </div>
+      <br/>
+
+      <div class="questionFormat">
       <input type="radio" name="answer" value="${question.answers[3]}">
-      <br>
-      <button type="submit">Submit your answer</button>
+      <label> ${question.answers[3]}</label>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+    <div class="submit-button">
+    <button type="submit">Give it a go!</button>
+    </div>
+    <br/>
   </form>
 </div>`;
   return questionPage;
 }
 
 function generateFeedback() {
-  let failedAnswer = `<div class="failed">
-  <p>So close! The right answer is actually ${store.questions[store.questionNumber].correctAnswer} </p>
-<br/><button class="next-question">Next question</button></div>`;
+  let failedAnswer =
+    `<div class="failed">
+  <br/>
+  <p>Blimey! That isn't right Mate.</p>
+  <p>The right answer is actually : ${store.questions[store.questionNumber].correctAnswer} </p>
+  <br/>
+  <iframe src="https://giphy.com/embed/Y4sY1loVstZUPP5nHR" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+  <br/>
+  <br/>
+  <div class ="failed-button">
+  <button class="next-question">Cheers! Next Question</button>
+  </div>
+  <br/>
+  </div>`;
   return failedAnswer;
 }
 
 function generateEndPage() {
-  let endPage = `<div class="endPage">
-  <h1>Great Job!</h1>
-  <p>Score:${store.score}</p>
+  let endPage =
+    `<div class="endPage">
+  <br/>
+  <h1>Jammy Job!</h1>
+  <h2>You got ${store.score} correct!</h2>
   <button class="play-again-button">
     <span class="button-label">Play Again!</span>
   </button>
